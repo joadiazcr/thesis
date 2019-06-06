4308c7a1be4fe7800fb99681e60b2062dadae0f8
jorgediaz <dejorge@slac.stanford.edu>
2019-06-05 17:31:36 -0700

    add fill and step detune test

--- cavity_model.py
+++ cavity_model.py
@@ -6,7 +6,10 @@ import matplotlib.pyplot as plt
 # Model for the accelerating voltage inside a cavity
 # based on CMOC Equation 3.13
 # Function that returns dS/dt
-def model(z, t, RoverQ, Qg, Q0, Qprobe, bw, Kg, Ib, foffset):
+def model(z, t, RoverQ, Qg, Q0, Qprobe, bw, Kg, Ib, foffset, Kg_s = 0.0, foffset_s = 0.0):
+    if t<Kg_s: Kg = 0.0
+    if t<foffset_s: foffset = 0.0
+
     Rg = RoverQ * Qg
     Kdrive = 2 * np.sqrt(Rg)
     bw = 104
@@ -27,7 +30,7 @@ def model(z, t, RoverQ, Qg, Q0, Qprobe, bw, Kg, Ib, foffset):
 
 
 # initial condition
-z0 = [0, 0, 0]  # y_r, y_i and theta
+z0 = [0, 0, 0]  # y_r, y_i, theta
 
 # time points
 t = np.linspace(0, 0.2, 1000)  # 0.2 seconds
@@ -42,6 +45,7 @@ RoverQ_pi = 1036
 Qg_pi = 4.0 * 10**7
 Q0_pi = 2.7 * 10**10
 Qprobe_pi = 2.0 * 10**9
+
 args_pi = (RoverQ_pi, Qg_pi, Q0_pi, Qprobe_pi, bw, Kg, Ib, foffset)
 y_pi = odeint(model, z0, t, args_pi)
 v_pi = (y_pi[:, 0] + 1j*y_pi[:, 1])*np.exp(1j*y_pi[:, 2])
@@ -106,21 +110,65 @@ plt.show()
 
 # Testing frequency offsets
 # time points
+Kg = 1
+Ib = 0
 t = np.linspace(0, 1, 10000)  # 0.2 seconds
 foffset = [0, 10, 20, 100]
 for f in foffset:
-	args_pi = (RoverQ_pi, Qg_pi, Q0_pi, Qprobe_pi, bw, Kg, Ib, f)
-	y_pi_f = odeint(model, z0, t, args_pi)
-	v_pi_f = (y_pi_f[:, 0] + 1j*y_pi_f[:, 1])*np.exp(1j*y_pi_f[:, 2])
+    args_pi = (RoverQ_pi, Qg_pi, Q0_pi, Qprobe_pi, bw, Kg, Ib, f)
+    y_pi_f = odeint(model, z0, t, args_pi)
+    v_pi_f = (y_pi_f[:, 0] + 1j*y_pi_f[:, 1])*np.exp(1j*y_pi_f[:, 2])
 
-	# plot results
-	plt.plot(np.real(v_pi_f), np.imag(v_pi_f), label='basis offset=%s Hz' %f)
+    # plot results
+    plt.plot(np.real(v_pi_f), np.imag(v_pi_f), label='basis offset=%s Hz' % f)
 
 plt.title("Cavity Step Response: Drive @ "+r'$\omega_{ref}$')
 plt.xlabel(r'$\Re (\vec V_{\mu})$ [V]')
 plt.ylabel(r'$\Im (\vec V_{\mu})$ [V]')
-plt.ylim([-0.5e4, 3e4])
-plt.xlim([-0.5e4, 5e4])
+plt.ylim([-0.5e5, 3e5])
+plt.xlim([-0.5e5, 5e5])
 plt.axes().set_aspect('equal')
 plt.legend()
+plt.show()
+
+
+# Testing fill and step on detune frequency
+nt = 10000
+t = np.linspace(0, 0.5, nt)  # 0.5 seconds
+Kg = 1.0
+Ib = 0.0
+Kg_s = 0.05
+foffset = [100.0, 200.0]
+foffset_s = 0.2
+
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+lns = []
+colors = ['c','m']
+
+for idx, f in enumerate(foffset):
+    args_pi = (RoverQ_pi, Qg_pi, Q0_pi, Qprobe_pi, bw, Kg, Ib, f, Kg_s, foffset_s)
+    y_pi_d = odeint(model, z0, t, args_pi)
+    v_pi_d = (y_pi_d[:, 0] + 1j*y_pi_d[:, 1])*np.exp(1j*y_pi_d[:, 2])
+
+    # plot results
+    lns += ax1.plot(t, np.abs(v_pi_d), label='Cavity Field ('+r'$\Delta f_1$ = %s Hz' % f+')')
+
+    delta_f = np.ones(nt)*f
+    delta_f[0:int(nt*0.4)] = 0.0
+    lns += ax2.plot(t, delta_f, '-%s' % colors[idx], label=r'$\Delta f_1$ = %s Hz' % f)
+
+drive_in = np.ones(nt)
+drive_in[0:int(nt*0.1)] = 0.0
+
+lns += ax1.plot(t, drive_in*max(np.abs(v_pi_d)), label='Drive')
+labs = [l.get_label() for l in lns]
+
+plt.title("Cavity Step Response: 1 pC Beam step")
+ax1.set_xlabel('Time [s]')
+ax1.set_ylabel(r'$| \vec V_{\rm acc}|$ [V]')
+ax2.set_ylabel('Frequency [Hz]')
+ax1.set_ylim(0, 6e5)
+ax2.set_ylim(0, foffset[1]*1.5)
+ax1.legend(lns, labs, loc=0)
 plt.show()
