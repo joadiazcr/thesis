69b17fe438103b6ee5d75860fee2b5de1aa9be10
Diaz-Cruz <dejorge@slac.stanford.edu>
2020-07-27 19:24:57 -0700

    Add script to plot optimization results

---
+++ examples/plot_opt_results.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+
+def load_data(file):
+    gain_s = []
+    rmse_s = []
+    results = open(file, 'r')
+    for line in results:
+        if 'Optimization method' in line:
+            method = line.split(':')[1]
+            print('The optimization method is ', method)
+        elif 'Solution' not in line and  '0-dB' not in line:
+            gain_s.append(float(line.split()[0]))
+            rmse_s.append(float(line.split()[1]))
+    print(gain_s)
+    print(rmse_s)
+    return method, gain_s, rmse_s
+
+def plot_opt_results(method, gain_s, rmse_s):
+    print('Plotting results...')
+    plt.title(method)
+    plt.plot(gain_s, rmse_s, '*')
+    plt.xlabel('0-dB crossing [Hz]')
+    plt.ylabel('RMSE [V]')
+    plt.show()
+
+if __name__ == "__main__":
+    des = 'Plot results of optimization'
+    parser = argparse.ArgumentParser(description=des)
+    parser.add_argument('-f', '--file', dest='log_file', required=True,
+                        help='File with opt results to be plotted')
+
+    args = parser.parse_args()
+
+    method, gain_s, rmse_s = load_data(args.log_file)
+    plot_opt_results(method, gain_s, rmse_s)
