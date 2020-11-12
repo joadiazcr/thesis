b7de57f74eb7e7a6416b58331f88a79068a6c86a
Diaz-Cruz <dejorge@slac.stanford.edu>
2020-11-12 10:59:20 -0800

    add functions to process HOM data (calcualte mean and std of the peak) and to plot V101 vs HOM

--- HOM_FAST_20201106/process_FAST_data.py
+++ HOM_FAST_20201106/process_FAST_data.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 import scipy.io as sio
+import csv
 
 
 def basic_file_info(data):
@@ -88,6 +89,74 @@ def plot_raw_HOMs(data, mode):
     plt.show()
 
 
+def process_HOM_data(data):
+    HOMs = data['HOMs']
+    num_HOMs = HOMs.shape[0]
+    reps = HOMs.shape[1]
+    reps=26
+
+    fig, axs = plt.subplots(2, 2)
+    homs_peak_based_mean = np.zeros(num_HOMs)
+    homs_peak_based_std = np.zeros(num_HOMs)
+    for hom in range(0,num_HOMs-2):
+        homs_peaks_based = np.zeros(reps)
+        for rep in range(0,reps):
+            hom_baseln = np.mean(np.concatenate((HOMs[hom][rep][400:600], HOMs[hom][rep][900:1000])))
+            hom_based = HOMs[hom][rep] - hom_baseln
+            axs[int(hom/2), hom%2].plot(hom_based)
+            homs_peaks_based[rep] = np.min(hom_based)
+        homs_peak_based_mean[hom] = np.mean(homs_peaks_based)
+        homs_peak_based_std[hom] = np.std(homs_peaks_based)
+        axs[int(hom/2), hom%2].set_xlim(740, 810)
+
+    print('homs_peak_based_mean = ', homs_peak_based_mean)
+    print('homs_peak_based_std = ', homs_peak_based_std)
+
+    fig.suptitle('225 pC, 50 b, 0 Ampl, V101 = 1.04 A \n AllData_2020-11-06-19-27-15.mat', fontsize=16) # Hardcoded title
+    axs[0,0].set_title('CC1 Upstr Peak_Mean = %.3f Peak_STD = %.4f' % (homs_peak_based_mean[0], homs_peak_based_std[0]))
+    axs[0,1].set_title('CC1 Dwnstr Peak_Mean = %.3f Peak_STD = %.4f' % (homs_peak_based_mean[1], homs_peak_based_std[1]))
+    axs[1,0].set_title('CC2 Upstr Peak_Mean = %.3f Peak_STD = %.4f' % (homs_peak_based_mean[2], homs_peak_based_std[2]))
+    axs[1,1].set_title('CC2 Dwnstr Peak_Mean = %.3f Peak_STD = %.4f' % (homs_peak_based_mean[3], homs_peak_based_std[3]))
+    plt.legend()
+    plt.show()
+
+
+def V101_HOM(data_file):
+    with open(data_file) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        v101 = []
+        hom1 = []
+        hom2 = []
+        hom3 = []
+        hom4 = []
+        hom1_std = []
+        hom2_std = []
+        hom3_std = []
+        hom4_std = []
+        for row in readCSV:
+            if 'V101' not in row:
+                v101.append(float(row[0]))
+                hom1.append(float(row[1]))
+                hom2.append(float(row[2]))
+                hom3.append(float(row[3]))
+                hom4.append(float(row[4]))
+                hom1_std.append(float(row[5]))
+                hom2_std.append(float(row[6]))
+                hom3_std.append(float(row[7]))
+                hom4_std.append(float(row[8]))
+
+    plt.errorbar(v101,hom1,yerr=hom1_std,label='CC1 Upstr')
+    plt.errorbar(v101,hom2,yerr=hom2_std,label='CC1 Dwnstr')
+    plt.errorbar(v101,hom3,yerr=hom3_std,label='CC2 Upstr')
+    plt.errorbar(v101,hom4,yerr=hom4_std,label='CC2 Dwnstr')
+    plt.xlabel('V101 [A]')
+    plt.ylabel('HOM Signal')
+    plt.title('225 pC, 50b, 0Ampl') # Hardcoded title
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
 if __name__ == "__main__":
     des = 'Procces and plot FAST data'
     parser = argparse.ArgumentParser(description=des)
@@ -99,18 +168,27 @@ if __name__ == "__main__":
                         help='Plot BPM data')
     parser.add_argument('-tor', action="store_true",
                         help='Plot toroid data')
+    parser.add_argument('-p', action="store_true",
+                        help='Process HOM data')
+    parser.add_argument('-vh', action="store_true",
+                        help='Plot V101 vs HOMs')
     parser.add_argument('-m', metavar='mode', dest='mode', type=int,
                         choices=range(0, 7), default=0,
                         help='Select the device number. 0 to plot all devices')
 
     args = parser.parse_args()
 
+    if args.vh:
+        V101_HOM(args.data_file)
+        exit()
+
     data = sio.loadmat(args.data_file)
     HOMsList = ['CC1 Upstream', 'CC1 Downstream', 'CC2 Upstream',
                 'CC2 Downstream', 'C1IW1A?', 'C1IW1B?']
     data['HOMs_List'] = HOMsList
-
     basic_file_info(data)
+    if args.p:
+        process_HOM_data(data)
     if args.tor:
         plot_toroids(data, args.mode)
     if args.bpm:
