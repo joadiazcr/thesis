bcdc4ae95592790a6e7c8102f392544c79d4a61c
Jorge Diaz <jorgealbertodiazcruz@gmail.com>
2022-04-26 15:15:31 -0700

    flake8

--- examples/pi_optimization.py
+++ examples/pi_optimization.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import sys
 import os
 import argparse
@@ -7,13 +6,15 @@ from scipy.optimize import minimize
 from datetime import datetime
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
-from cavity_step_response import cavity_step
+from cavity_step_response import cavity_step as cav_stp
 from pi_gain_analysis import error, rmse
 
 
-# Example from: https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html 
+# Example from:
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
 def test_function():
     print('\nSLSQP')
+
     def fun(x):
         print(x)
         y = (x[0] - 1)**2 + (x[1] - 2.5)**2
@@ -31,8 +32,8 @@ def test_function():
 def cavity_step_rmse(s_gbw):
     start = datetime.now()
     # Some settings for the simulation
-    tf = 0.03 # Total simulation time
-    nom_grad = 1.6301e7 # V/m. Nominal gradient of LCLS-II cavity
+    tf = 0.03  # Total simulation time
+    nom_grad = 1.6301e7  # V/m. Nominal gradient of LCLS-II cavity
     beam = False
     detuning = True
     noise = True
@@ -44,9 +45,10 @@ def cavity_step_rmse(s_gbw):
     detuning_end = tf
 
     # 1. Simulate cavity response with given gains
-    t, cav_v, sp, fwd, Kp_a, Ki_a, e, ie = cavity_step(tf, nom_grad, feedforward, noise, noise_psd,\
-                                                             beam, beam_start, beam_end, detuning,\
-                                                             detuning_start, detuning_end, stable_gbw=s_gbw)
+    args = [tf, nom_grad, feedforward, noise, noise_psd, beam, beam_start,
+            beam_end, detuning, detuning_start, detuning_end]
+    t, cav_v, sp, fwd, Kp_a, Ki_a, e, ie = cav_stp(args,
+                                                   stable_gbw=s_gbw)
 
     # 2. Calculate error
     err = error(sp, np.abs(cav_v))
@@ -68,7 +70,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    s_gbw = 40000 # Initial guess
+    s_gbw = 40000  # Initial guess
 
     if args.test:
         print('\nTesting cavity_step_rmse...')
@@ -78,6 +80,6 @@ if __name__ == "__main__":
     else:
         print('\nNelder-Mead')
         print("0-dB crossing\t\trmse\t\tExec Time")
-        res = minimize(cavity_step_rmse, s_gbw, method='SLSQP', 
+        res = minimize(cavity_step_rmse, s_gbw, method='SLSQP',
                        tol=1e-1, options={'disp': True, 'maxiter': 1})
         print('Solution =', res.x)
