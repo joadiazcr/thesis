11ba4f9a1926b18a297f002b9308413a9b019759
Jorge Diaz <dejorge@slac.stanford.edu>
2026-04-02 16:29:08 -0700

    Complete update to concat.py

--- NANC/concat.py
+++ NANC/concat.py
@@ -1,13 +1,35 @@
 import numpy as np
-import glob
+from pathlib import Path
+import sys
 
-# Get all txt files in folder
-files = sorted(glob.glob("/home/dejorge/Documents/NANC/shift_20251106/res_CM19_cav2_*"))
 
-# Load each file as a column
-arrays = [np.loadtxt(f) for f in files]
+def stack_res_files(file_list, output_file):
+    arrays = []
+    for f in file_list:
+        path = Path(f)
+        if path.is_file():
+            data = np.loadtxt(f)
+            print(f'{data.shape} {f}')
+            arrays.append(data)
+        else:
+            sys.exit(f'Error: {f} does not exist!')
+            exit()
 
-# Stack horizontally (columns)
-data = np.column_stack(arrays)
+    data_total = np.column_stack(arrays)
+    np.savetxt(output_file, data_total, fmt="%.6f")
+    print(f'\n{data_total.shape} {output_file}')
 
-np.savetxt("/home/dejorge/Documents/NANC/shift_20251106/combined_cm19_cav2", data, fmt="%.6f")
+
+if __name__ == "__main__":
+
+    from argparse import ArgumentParser
+
+    desc = 'Script to stack files created by res_data_acq.py.'
+    parser = ArgumentParser(description=desc)
+    parser.add_argument("-f", "--file_list", nargs="+", required=True,
+                        help="List of files to stack")
+    parser.add_argument("-o", "--output_file", required=True,
+                        help="Path and name for output file")
+    args = parser.parse_args()
+
+    stack_res_files(args.file_list, args.output_file)
