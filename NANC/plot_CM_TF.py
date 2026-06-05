6b9030529ab4e923d5912f24ef3b77d4b9210969
Jorge Diaz <dejorge@slac.stanford.edu>
2026-06-04 17:14:18 -0700

    Flake8

--- NANC/plot_CM_TF.py
+++ NANC/plot_CM_TF.py
@@ -31,42 +31,44 @@ if __name__ == "__main__":
     files = [f for f in dir_path.iterdir() if f.is_file()]
     files.sort(key=lambda f: f.name)
 
-    nominal_low_freq = 1
-    nominal_high_freq = 80
+    nom_low_freq = 1
+    nom_high_freq = 80
     amp_max = 0
     fig1 = plt.figure(figsize=(19.2, 10.8), dpi=100)
     fig2 = plt.figure(figsize=(19.2, 10.8), dpi=100)
     for file in files:
         metadata = read_metadata(file)
         cav_num = metadata['CAV1']['cav_number']
-        
+
         parts = file.name.split('_')
         low_freq = int(parts[2].replace('Hz', ''))
         high_freq = int(parts[3].replace('Hz', ''))
 
-        if low_freq == nominal_low_freq and high_freq == nominal_high_freq:
+        if low_freq == nom_low_freq and high_freq == nom_high_freq:
             print(file.name)
             res_data = ResData(file, args.count, args.wsp)
-            amp_max_temp = np.max(res_data.tf_mag[nominal_low_freq:nominal_high_freq*3])
-            if amp_max_temp > amp_max:
-                amp_max = amp_max_temp
+            amp_max_t = np.max(res_data.tf_mag[nom_low_freq:nom_high_freq*3])
+            if amp_max_t > amp_max:
+                amp_max = amp_max_t
                 print(amp_max)
             plt.figure(fig1.number)
-            plt.plot(res_data.p_dac.xf, res_data.tf_mag, linewidth=2, label=f'cav{cav_num}')
+            plt.plot(res_data.p_dac.xf, res_data.tf_mag,
+                     linewidth=2, label=f'cav{cav_num}')
             plt.figure(fig2.number)
-            plt.plot(res_data.p_dac.xf, res_data.tf_phs, linewidth=2, label=f'cav{cav_num}')
-    
+            plt.plot(res_data.p_dac.xf, res_data.tf_phs,
+                     linewidth=2, label=f'cav{cav_num}')
+
     plt.figure(fig1.number)
     plt.xlabel('Frequency [Hz]')
     plt.ylabel('A [Hz/V]')
     plt.ylim(0, 1.1 * amp_max)
-    plt.xlim(nominal_low_freq, nominal_high_freq)
+    plt.xlim(nom_low_freq, nom_high_freq)
     plt.legend()
 
     plt.figure(fig2.number)
     plt.xlabel('Frequency [Hz]')
     plt.ylabel('Phase [rad]')
     plt.ylim(-2*np.pi, 2*np.pi)
-    plt.xlim(nominal_low_freq, nominal_high_freq)
+    plt.xlim(nom_low_freq, nom_high_freq)
     plt.legend()
     plt.show()
