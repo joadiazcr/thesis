c6724d7b933725b2335f7a166e2f043850911159
Jorge Diaz <dejorge@slac.stanford.edu>
2026-08-24 17:35:29 -0700

    Add functions to plot waveforms and detuning from ca monitor data

--- NANC/vts/plot_ca_data.py
+++ NANC/vts/plot_ca_data.py
@@ -1,8 +1,18 @@
 from dataclasses import dataclass, field
+import itertools
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+def flatten_lists(x):
+    shape = np.shape(x)
+    if len(shape) > 1:
+        return list(itertools.chain.from_iterable(x))
+    else:
+        return x
+
 
 @dataclass
 class CamonitorFile():
@@ -30,6 +40,46 @@ class CamonitorFile():
 
         self.df = pd.DataFrame(self.pv_dict.items(), columns=['PV', 'Value'])
         self.df.set_index('PV', inplace=True)
+        self.df['Value'] = self.df['Value'].apply(flatten_lists)
+
+    def time_axis(self):
+        decim_rf = self.df.loc["ACQ_DECIM", 'Value'][0]
+        ts_rf = (14 * 2 * 33 * decim_rf) / 1.32e9
+        self.taxis_rf = np.arange(len(self.df.loc[f"CAV:IWF", 'Value'])) * ts_rf
+
+        decim_rc = 1 # Default. Check!
+        ts_rc = decim_rc / 2.0e3 # 2 KHz is the maximun sampling rate
+        self.taxis_rc = np.arange(len(self.df.loc[f"PZT:DF:WF", 'Value'])) * ts_rc
+
+    def plot_wf(self, pv):
+        i_wf = self.df.loc[f"{pv}:IWF", 'Value']
+        q_wf = self.df.loc[f"{pv}:QWF", 'Value']
+        wf = np.array(i_wf) + 1j * np.array(q_wf)
+        fig, ax = plt.subplots(4, 1, figsize=(20, 12), sharex=True)
+        ax[0].plot(self.taxis_rf, np.abs(wf))
+        ax[0].axhline(y=self.df.loc[f"ADES", 'Value'][0], color='r', linestyle='--', label='ADES')
+        ax[1].plot(self.taxis_rf, np.degrees(np.angle(wf)))
+        ax[2].plot(self.taxis_rf, i_wf)
+        ax[3].plot(self.taxis_rf, q_wf)
+        ax[0].set_xlim(0, self.taxis_rf[-1])
+        ax[0].set_ylabel('Amplitude [MV]')
+        ax[1].set_ylabel('Phase [rad]')
+        ax[2].set_ylabel('I [MV]')
+        ax[3].set_ylabel('Q [MV]')
+        ax[3].set_xlabel('Time [s]')
+        ax[0].legend()
+        plt.show()
+
+    def plot_detuning(self):
+        det_wf = self.df.loc["DF:WF", 'Value']
+        det_pzt_wf = self.df.loc["PZT:DF:WF", 'Value']
+        plt.plot(self.taxis_rc, det_pzt_wf, label='PZT Detuning')
+        plt.plot(self.taxis_rf, det_wf, label='RFS Detuning')
+        plt.xlabel('Time [s]')
+        plt.ylabel('Detuning [Hz]')
+        plt.title('Cavity Detuning')
+        plt.legend()
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -53,11 +103,8 @@ if __name__ == "__main__":
 
     ca_file = CamonitorFile(args.datafile, args.prefix)
     ca_file.load_data()
-    print(ca_file.df.head(20))
+    ca_file.time_axis()
+    print(ca_file.df.head(40))
 
-    df_wf_raw = ca_file.df.loc['CAV:IWF', 'Value']
-    df_wf = np.concatenate(df_wf_raw).tolist()
-    print(len(df_wf))
-    print(df_wf[:20])
-    plt.plot(df_wf)
-    plt.show()
+    #ca_file.plot_wf('CAV')
+    ca_file.plot_detuning()
